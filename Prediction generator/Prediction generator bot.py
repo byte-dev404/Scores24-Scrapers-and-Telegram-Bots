b6035ca8add0d18ad05db2f0238e980ce03b4e09
Dev-404 <zendiagogamingbackup@gmail.com>
2026-01-18 09:02:53 +0530

    Added selectable options

--- Prediction generator/Prediction generator bot.py
+++ Prediction generator/Prediction generator bot.py
@@ -11,11 +11,16 @@ bot_token = os.getenv("PREDICTION_GENERATOR_BOT_TOKEN")
 ''' Or simply paste it here '''
 # bot_token = "~~Enter~~your~~bot~~token~~here~~" # but never forget to remove it before commiting to github
 
-
 if not bot_token:
     raise TypeError("Error: bot token is missing\nAdd your bot token right after imports then try again.")
 
+# Predefined messages for commands
 start_msg = "Hi, I'm Scores24 Prediction Generator Bot.\nDo /help to see the full list of commands"
 help_msg = "Here's the full list of commands that might help.\n\n/start - To start the conversation with bot\n/help - To see all commands and get help\n/contact - To contact my creator\n/generate_prediction - To generate predictions\n\nIf the above doesn't help, you might wanna contact my creator, for that do /contact"
 contact_msg = "Contact my creator Mr. Vishwas Batra,\nHere on LinkedIn: https://www.linkedin.com/in/vishwas-batra/"
 unknown_msg = "Sorry, I didn't understand that command, maybe because this command is not defined.\nContact the developer via /contact command, if you want to add new features."
+
+# All selectable options
+mode_options = ["Best", "Custom"]
+time_options = ["all", "today", "tomorrow"]
+sport_options = ["all", 'soccer', 'ice-hockey', 'basketball', 'tennis', 'futsal', 'mma', 'snooker', 'baseball', 'american-football', 'csgo', 'volleyball', 'rugby', 'handball', 'boxing',]
