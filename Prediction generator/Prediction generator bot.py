9cbb54615fb23ba2a23f10a94beac066b415b0ea
Dev-404 <zendiagogamingbackup@gmail.com>
2026-01-18 11:16:06 +0530

    Added logging

--- Prediction generator/Prediction generator bot.py
+++ Prediction generator/Prediction generator bot.py
@@ -96,3 +96,6 @@ json_data = {
         'timezoneOffset': 330,
     },
 }
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
